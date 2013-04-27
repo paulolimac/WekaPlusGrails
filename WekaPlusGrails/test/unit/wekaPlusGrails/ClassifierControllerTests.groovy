0a56abbfa7de8bd8fe7f959d5516492eaa834bd8
Paulo de Lima Cavalcanti <email@paulolimac.com>
2013-04-27 13:40:07 -0300

    Added Classifier domain and controller classes
    Added Classifier domain and controller classes
    
    Signed-off-by: Paulo de Lima Cavalcanti <email@paulolimac.com>

---
+++ WekaPlusGrails/test/unit/wekaPlusGrails/ClassifierControllerTests.groovy
@@ -0,0 +1,17 @@
+package wekaPlusGrails
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ClassifierController)
+class ClassifierControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
